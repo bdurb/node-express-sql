01a49077abbec25b2f5c4d2588b7c2588fd5b669
bdurb <brian@rpmgreenville.com>
2018-07-30 18:44:09 -0700

    set up get route for posts

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ server.use(express.json());
 
 const PORT = 8000
 
-
+server.get('/posts', (req, res) => {
+  db.find()
+  .then(posts => {
+    res.json(posts)
+  })
+  .catch(() => {
+    res.status(500).json({ error: "There was an error while saving the post to the database" })
+  })
+})
 
 server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
