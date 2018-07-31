b761fa9287cb7f72ddf5ef20757503a3305a70d0
bdurb <brian@rpmgreenville.com>
2018-07-30 18:40:08 -0700

    set up basic express server

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-
+const express = require('express');
 const db = require('./data/db.js');
+
+const server = express();
+server.use(express.json());
+
+const PORT = 8000
+
+
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`)
+})
 
 
