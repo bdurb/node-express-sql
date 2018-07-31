c1f3228728ec3eadb14f8024e0d8fdcb0536d103
bdurb <brian@rpmgreenville.com>
2018-07-30 19:15:45 -0700

    set up get and get by id routes

--- server.js
+++ server.js
@@ -16,6 +16,34 @@ server.get('/posts', (req, res) => {
   })
 })
 
+server.get('/posts/:id', (req, res) => {
+  const {id} = req.params
+  db.findById(id)
+  .then(post => {
+    if (post.length < 1) {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    } else {
+      res.json(post)
+    }
+  })
+  .catch(() => {
+    res.status(500).json({ error: "The post information could not be retrieved." })
+  })
+})
+
+// server.post('/posts', (req, res) => {
+//   const post = req.body
+//   if (!post.title || !post.contents) {
+//     res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+//   }
+//   db.insert(post)
+//   .then(post => {
+
+//   })
+// })
+
+
+
 server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
 })
